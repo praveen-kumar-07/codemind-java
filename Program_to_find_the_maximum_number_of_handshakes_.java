037aefb36cd6df6aec1c9501f9510180d9879854
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2024-01-06 11:48:49 +0530

    import java.util.*; public class Solution{     public static void main(String[] praveen){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=(a*(a-1))/2;         System.out.print(b);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] praveen){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=(a*(a-1))/2;
+        System.out.print(b);
+    }
+}
