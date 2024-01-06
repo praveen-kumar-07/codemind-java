00dd130aa993a65de399375760fbee9e4ff61a9e
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2024-01-06 11:57:29 +0530

    import java.util.*; public class Solution{     public static void main(String[] praveen){         Scanner sc=new Scanner(System.in);         int a= sc.nextInt();         int b=sc.nextInt();         int c=a%b;         System.out.println(c);     } }

---
+++ Modulus.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] praveen){
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int c=a%b;
+        System.out.println(c);
+    }
+}
