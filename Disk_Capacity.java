c7b7d27bb4b2b99a8c3e70c2bdde093890a81359
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2024-01-06 11:42:44 +0530

    import java.util.*; public class Solution{     public static void main(String[] praveen){         Scanner sc= new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         long d=2*a*b*c*512;         System.out.println(d);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] praveen){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        long d=2*a*b*c*512;
+        System.out.println(d);
+        
+    }
+}
