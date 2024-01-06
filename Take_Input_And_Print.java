f91facbe5de9d7a0ef4983b43646218f127f125c
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2024-01-06 11:54:18 +0530

    import java.util.*; public class Solution{     public static void main(String[] praveen){         Scanner sc=new Scanner(System.in);         String a=sc.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] praveen){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println(a);
+    }
+}
