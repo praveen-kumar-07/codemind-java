e71e524e6b1a61f2d38e8ec807b6dcebae86d5ef
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2024-01-10 11:02:44 +0530

    import java.util.*; public class Solution{     public static void main(String[] praveen){         Scanner sc= new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         System.out.println(a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] praveen){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a*b);
+    }
+}
