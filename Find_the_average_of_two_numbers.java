ffd2bd2c993eb7c615d60439749c92657299e54d
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2024-01-11 10:47:50 +0530

    import java.util.*; public class Solution{     public static void main(String[] praveen){         Scanner sc=new Scanner(System.in);          double a=sc.nextDouble();          double b=sc.nextDouble();
             System.out.printf("%.4f",(a+b)/2);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] praveen){
+        Scanner sc=new Scanner(System.in);
+         double a=sc.nextDouble();
+         double b=sc.nextDouble();
+         
+         System.out.printf("%.4f",(a+b)/2);
+    }
+}
