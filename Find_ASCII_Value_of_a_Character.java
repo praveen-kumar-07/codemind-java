d02ea64dbf311e8024ad7d7120fd284227f9abdc
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2024-01-11 10:41:59 +0530

    import java.util.*; public class Solution{     public static void main(String[] praveen){         Scanner sc=new Scanner(System.in);         char a=sc.next().charAt(0);         System.out.println((int)a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] praveen){
+        Scanner sc=new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        System.out.println((int)a);
+    }
+}
