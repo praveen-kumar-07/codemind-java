00c3a35e6c792d2b3600c60f6c72189c87bdc330
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2024-01-13 13:04:42 +0530

    import java.io.*; import java.util.*;
    public class Solution {
    
        public static void main(String[] args) {
            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
            Scanner sc=new Scanner(System.in);
            int a=sc.nextInt();
            double b=32+(a*(1.8));
            System.out.printf("%.2f",b);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=32+(a*(1.8));
+        System.out.printf("%.2f",b);
+    }
+}
